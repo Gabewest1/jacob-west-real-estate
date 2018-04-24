a8f377acae96676b09f07fca5a54956d51fc94fa
Gabriel West <gabe_west1@yahoo.com>
2018-04-24 01:43:32 -0500

    Added eslint with airbnb styles. Did add some modifications to the eslint rules.

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "semi": ["error", "never"],
+        "quotes": ["error", "double"],
+        "react/jsx-filename-extension": ["off"],
+        "linebreak-style": ["off"],
+        "indent": ["error", 4],
+        "react/jsx-indent": ["error", 4],
+        "react/prefer-stateless-function": ["off"]
+    }
+};
