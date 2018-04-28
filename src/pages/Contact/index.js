9ee13c95e1405fb88b942c56ac97484bc49f50cc
Gabriel West <gabe_west1@yahoo.com>
2018-04-27 20:12:59 -0500

    Created <ContactPage/> and made it responsive. Still needs functionality and minor style tweaks.

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,16 +1,123 @@
 import React from "react"
 import styled from "styled-components"
+import ContactForm from "../../components/ContactForm"
+import { WIDTH_OF_ALL_CONTAINERS } from "../../constants"
 
 class Contact extends React.Component {
     render() {
         return (
             <ContactView {...this.props}>
-                Contact Here
+                <SiteWrapper>
+
+                    <Header>Get In Touch</Header>
+
+                    <Body>
+                        <LeaveMessageWrapper>
+                            <SubHeader>Leave A Message</SubHeader>
+                            <ContactMessage>
+                                We promise to never share personal information with
+                                any thieves, hackers or third party services.
+                            </ContactMessage>
+                            <ContactForm />
+                        </LeaveMessageWrapper>
+
+                        <ContactDirectlyWrapper>
+                            <SubHeader>Contact Directly</SubHeader>
+
+                            <ContactInfoContainer>
+                                <ContactWrapper>
+                                    <ContactTitle>Personal Phone</ContactTitle>
+                                    <ContactInfo>512 669 9649</ContactInfo>
+                                </ContactWrapper>
+
+                                <ContactWrapper>
+                                    <ContactTitle>Email</ContactTitle>
+                                    <ContactInfo>jacobwest1@austinelite.com</ContactInfo>
+                                </ContactWrapper>
+
+                                <ContactWrapper>
+                                    <ContactTitle>Office Address</ContactTitle>
+                                    <ContactInfo>2321 Loquat Ln Round Rock, Texas 78664</ContactInfo>
+                                </ContactWrapper>
+
+                                <ContactWrapper>
+                                    <ContactTitle>Office Phone</ContactTitle>
+                                    <ContactInfo>512 669 9649</ContactInfo>
+                                </ContactWrapper>
+                            </ContactInfoContainer>
+                        </ContactDirectlyWrapper>
+                    </Body>
+                </SiteWrapper>
             </ContactView>
         )
     }
 }
 
+const Header = styled.h1`
+    text-align: center;
+    padding: 40px 0 20px 0;
+    margin: 0;
+`
+const SubHeader = styled.h3``
+const ContactMessage = styled.p``
+const ContactTitle = styled.h3`
+    font-size: 16px;
+`
+const ContactInfo = styled.p`
+    font-size: 13px;
+`
+const ContactWrapper = styled.div`
+    margin-bottom: 10px;
+    
+    * {
+        margin: 0;
+    }
+`
+const ContactDirectlyWrapper = styled.div``
+const ContactInfoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    @media (min-width: 610px) {
+        flex-wrap: wrap;
+        flex-direction: row;
+        justify-content: space-between;
+
+        ${ContactWrapper}:nth-child(odd) {
+            flex-basis: 60%;
+        }
+    
+        ${ContactWrapper}:nth-child(even) {
+            flex-basis: 40%;
+        }
+    }
+
+    @media (min-width: 768px) {
+        margin-top: 79px;
+        flex-direction: column;
+    }
+`
+const LeaveMessageWrapper = styled.div`
+    max-width: 500px;
+
+    @media (min-width: 768px) {
+        margin-right: 60px;
+    }
+`
+const Body = styled.div`
+    display: flex;
+    flex-direction: column-reverse;
+    padding-bottom: 30px;
+
+    @media (min-width: 768px) {
+        flex-direction: row;
+    }
+
+`
+const SiteWrapper = styled.div`
+    width: ${WIDTH_OF_ALL_CONTAINERS};
+    margin: 0 auto;
+`
 const ContactView = styled.div``
 
 export default Contact
