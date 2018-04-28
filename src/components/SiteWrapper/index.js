b5a427e05385a2d86273e45249b42a294f8ff31f
michael.spencer1995 <michael.spencer1@g.austincc.edu>
2018-04-28 16:41:15 -0500

    fixed proptypes specs on site wrapper and section sub header

--- src/components/SiteWrapper/index.js
+++ src/components/SiteWrapper/index.js
@@ -1,13 +1,20 @@
 import React from "react"
 import styled from "styled-components"
+import PropTypes from "prop-types"
 import { WIDTH_OF_ALL_CONTAINERS } from "../../constants"
 
-const SiteWrapper = styled.div`
+
+const SiteWrapperView = styled.div`
     width: ${WIDTH_OF_ALL_CONTAINERS};
     margin: 0 auto;
 `
 
-export default props => (
-    <SiteWrapper {...props}>{props.children}</SiteWrapper>
+const SiteWrapper = props => (
+    <SiteWrapperView {...props}>{props.children}</SiteWrapperView>
 )
 
+SiteWrapper.propTypes = {
+    children: PropTypes.element,
+}
+
+export default SiteWrapper
