768690cee9cd0dba00375b21553f134bf4474fb6
Gabriel West <gabe_west1@yahoo.com>
2018-05-01 22:21:27 -0500

    Created Sell page. Still needs Motto Mortage information.

--- src/pages/Sell/index.js
+++ src/pages/Sell/index.js
@@ -1,17 +1,84 @@
 import React from "react"
 import styled from "styled-components"
 
+import SectionHeader from "../../components/SectionHeader"
+import SiteContainer from "../../components/SiteContainer"
+import SectionSubHeader from "../../components/SectionSubHeader"
+import ContactForm from "../../components/ContactFormAndInfo/ContactForm"
+
+import { PAGE_PADDING } from "../../constants"
+
 class Sell extends React.Component {
     render() {
         return (
             <SellView {...this.props}>
-                Sell Here
+                <SiteContainer>
+                    <SectionHeaderNoMargin blueWord="Sellers" />
+
+                    <SectionSubHeader>This is text for the sub header</SectionSubHeader>
+
+                    <Body>
+                        <Wrapper>
+                            <WhatsMyHomeWorth>
+                                <h3>How Jacob makes the proccess a breeze</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Quia voluptatibus tempora recusandae officiis.
+                                    Molestias alias ut magni natus amet similique quam quibusdam,
+                                    iure veniam vel, at quia blanditiis! Pariatur,
+                                    eligendi.
+                                </p>
+                            </WhatsMyHomeWorth>
+                            <GettingStarted>
+                                <h3>How to get started</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Quia voluptatibus tempora recusandae officiis.
+                                    Molestias alias ut magni natus amet similique quam quibusdam.
+                                </p>
+                            </GettingStarted>
+                            <ContactForm />
+                        </Wrapper>
+                        <div>
+                            This is where the current listings should go
+                        </div>
+                    </Body>
+                </SiteContainer>
             </SellView>
         )
     }
 }
 
+const SectionHeaderNoMargin = styled(SectionHeader)`
+    height: 60px;
+
+    * {
+        margin: 0;
+    }
+`
+const GettingStarted = styled.div``
+const WhatsMyHomeWorth = styled.div`
+
+    @media (min-width: 768px) {
+        margin-right: 25px;
+    }
+`
+const Wrapper = styled.div``
+const Body = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    @media (min-width: 768px) {
+        flex-direction: row;
+        justify-content: space-between;
+
+        > * {
+            flex-basis: 50%;
+        }
+    }
+`
 const SellView = styled.div`
+    ${PAGE_PADDING}
     background: white;
 `
 
