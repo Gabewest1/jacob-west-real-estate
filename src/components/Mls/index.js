f52c1c4e4f9702aca5b956a09cacddec29d8c0e4
Gabriel West <gabe_west1@yahoo.com>
2018-05-11 19:50:22 -0500

    Updated Mls to change the background color of the currently selected filter option

--- src/components/Mls/index.js
+++ src/components/Mls/index.js
@@ -5,13 +5,17 @@ import Baths from "./Baths"
 import Beds from "./Beds"
 import SquareFeet from "./SquareFeet"
 
+
+const BEDS = "Beds"
+const BATHS = "Baths"
+const SQ_FT = "Sq ft"
+
 class Mls extends React.Component {
     constructor() {
         super()
         this.defaultEditBox = {
-            left: 0,
             width: 0,
-            height: 0,
+            height: 55,
             top: "100%"
         }
         this.state = {
@@ -23,22 +27,25 @@ class Mls extends React.Component {
         const { editBox, activeFilter } = this.state
 
         return (
-            <MlsContainer {...this.props}>
-                <MlsForm onSubmit={this._handleSubmit} innerRef={container => this.container = container}>
+            <MlsContainer {...this.props} innerRef={container => this.container = container}>
+                <MlsForm onSubmit={this._handleSubmit}>
                     <MlsNumberInput placeholder="Enter City, Neighborhood, Address, Zip or MLS#" />
                     <MlsFilterOptions 
+                        isActive={activeFilter === BEDS}
                         onClick={this._handleFilterClick}
                         innerRef={beds => this.beds = beds}
                     >
                         Beds
                     </MlsFilterOptions>
                     <MlsFilterOptions 
+                        isActive={activeFilter === BATHS}
                         onClick={this._handleFilterClick}
                         innerRef={bath => this.bath = bath}
                     >
                         Baths
                     </MlsFilterOptions>
                     <MlsFilterOptions 
+                        isActive={activeFilter === SQ_FT}
                         onClick={this._handleFilterClick}
                         innerRef={squareFeet => this.squareFeet = squareFeet}
                     >
@@ -47,9 +54,9 @@ class Mls extends React.Component {
                     <MlsSubmitButton innerRef={button => this.button = button}>Find Homes</MlsSubmitButton>
 
                     <EditBox style={editBox}>
-                        {activeFilter === "Beds" ? <Beds />
-                            : activeFilter === "Baths" ? <Baths />
-                            : activeFilter ===  "Sq ft" ? <SquareFeet />
+                        {activeFilter === BEDS ? <Beds />
+                            : activeFilter === BATHS ? <Baths />
+                            : activeFilter ===  SQ_FT ? <SquareFeet />
                             : false
                         }
                     </EditBox>
@@ -64,23 +71,23 @@ class Mls extends React.Component {
         e.preventDefault()
 
         const activeFilter = e.target.textContent
-        const didClickAlreadyActiveFilter = activeFilter === this.state.activeFilter 
+        const didClickAlreadyActiveFilter = activeFilter === this.state.activeFilter
 
         if (didClickAlreadyActiveFilter) {
             this.setState({ activeFilter: false, editBox: this.defaultEditBox})
         } else {
-            const { x:containerX } = this.container.getBoundingClientRect()
+            const { x:containerX, width: containerWidth } = this.container.getBoundingClientRect()
             const { x:bedX } = this.beds.getBoundingClientRect()
-            const { x:buttonX, width:buttonWidth } = this.button.getBoundingClientRect()
 
             const left = bedX - containerX
-            const width = (buttonX + buttonWidth) - bedX
-            const height = 250
-            console.log(bedX, (buttonX + buttonWidth), width, height)
-            this.setState({ activeFilter, editBox: { ...this.state.editBox, left, width, height } })
+            const width = (containerX + containerWidth) - bedX
+            console.log(bedX, width)
+            this.setState({ activeFilter, editBox: { ...this.state.editBox, width } })
         }
     }
 }
+
+const SEARCH_BAR_WIDTH = 300
 
 const EditBox = styled.div`
     position: absolute;
@@ -88,6 +95,21 @@ const EditBox = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-around;
+    left: ${SEARCH_BAR_WIDTH}px;
+
+    > * {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        padding: 0 12px;
+
+        input {
+            height: 35px;
+            padding-left: 10px;
+            flex-basis: 48%;
+            box-sizing: border-box;
+        }
+    }
 `
 
 const MlsContainer = styled.div`
@@ -116,7 +138,7 @@ const MlsForm = styled.form`
 `
 
 const MlsNumberInput = styled.input`
-    width: 100%;
+    min-width: ${SEARCH_BAR_WIDTH}px;
     height: 47px;
     box-sizing: border-box;
     padding: 0;
@@ -151,7 +173,7 @@ const MlsFilterOptions = styled.button`
     justify-content: space-between;
     width: 300px;
     height: 47px;
-    background-color: #ececec;
+    background-color: ${({ isActive }) => isActive ? "lightgray" : "#ececec"};
     color: rgba(113, 113, 113, 0.832);
     border: rgba(170, 170, 170, 0.6) 1px solid; 
     border-left: none;   
